6ca63e053dc504701a58cd9be0f87f757a8d70a7
bomma.c <bommareddy.charitha@accolitedigital.com>
2022-12-27 15:37:40 +0530

    added header and footer

--- Beat/src/app/app.module.ts
+++ Beat/src/app/app.module.ts
@@ -1,32 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-
-import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
-
-@NgModule({
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import {MatCardModule} from '@angular/material/card';
+NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
     MatButtonModule,
     MatInputModule,
     ReactiveFormsModule,
-    MatIconModule
+    MatIconModule,
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
