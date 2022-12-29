62c427beb05bc34b410d7486010ac7820ecef009
nupur243 <nupur.agarwal@accolitedigital.com>
2022-12-29 14:55:48 +0530

    accessibility hidden and display functionality

--- Beat/src/app/material/material.module.ts
+++ Beat/src/app/material/material.module.ts
@@ -4,10 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
-
+import { MatSelectModule } from '@angular/material/select'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const materialComponents = [MatButtonModule,MatInputModule,
-                            MatIconModule,MatCardModule];
+                            MatIconModule,MatCardModule,
+                            MatSelectModule,
+                            MatFormFieldModule];
 
 @NgModule({
   imports: [materialComponents],
--- Beat/src/layout/header/header.component.ts
+++ Beat/src/layout/header/header.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit, OnInit{
 
+  accessString ="001111000000";
+  accessArray : string[]=[...this.accessString];
+  count =0;
+  element=document.getElementsByClassName("accessibility-options");
+
+
+  getEntity !: any;
+  childrenArray ! :any;
+  childrendivs!:any;
+  childCount=0;
+  accessChildren=0;
+  char ! :any;
+   accessibility() {
+
+    this.char=this.accessArray;
+    this.getEntity= this.element[0].children;
+  
+   //disable the whole employee, organization and project option for the user
+   if(this.char[0]==='0' && this.char[1]==='0' && this.char[2]==='0' && this.char[3]==='0')
+   {
+   this.getEntity[0].setAttribute("style","display:none");
+   }
+   
+   if(this.char[4]==='0' && this.char[5]==='0' && this.char[6]==='0' && this.char[7]==='0')
+   {
+   this.getEntity[1].setAttribute("style","display:none");
+   }
+
+   if(this.char[8]==='0' && this.char[9]==='0' && this.char[10]==='0' && this.char[11]==='0')
+   {
+   this.getEntity[2].setAttribute("style","display:none");
+   }
+
+
+   this.childrenArray= this.element[0].children[this.childCount].getElementsByTagName('li');
+   console.log(this.childrenArray);
+   
+   this.accessArray.forEach((char,index)=>{
+
+    console.log(index)
+    
+            if(index%4===0 && index>0)
+            {
+               this.childCount++;
+               this.accessChildren=0;
+               this.childrenArray= this.element[0].children[this.childCount].getElementsByTagName('li');
+            }
+
+            if(char==='0')
+            {
+                console.log(this.childrenArray[this.accessChildren])
+                this.childrenArray[this.accessChildren].setAttribute('style','display:none');
+            }
+            
+            this.accessChildren++;
+
+   })
+
+       
+
+  }
+  
+  
+ constructor(){
+ 
+  
+ }
+
+ ngAfterContentInit(){
+
+ }
+ngOnInit(){
+
+}
+  ngAfterViewInit(): void {
+    this.accessibility();
+  }  fun(){
+
+  }
+  
 }
